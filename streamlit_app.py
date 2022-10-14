21ee1af950836a6e58c746a93c9acc94dfbf1fb5
sivakamipalk <115802492+sivakamipalk@users.noreply.github.com>
2022-10-14 09:13:16 -0500

    Update streamlit_app.py
    added the meu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents new healthy diner")
+streamlit.title("Breakfast Menu")
+streamlit.title("Eggs")
+streamlit.title("Spinach")
+streamlit.title("Pancakes")
